d41724623bb3e6262c4c39729c2016b549c6e8be
Joris de Vrede <jorisdevrede@gmail.com>
2017-04-15 13:25:19 +0200

    Create install-ansible.sh

---
+++ install-ansible.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+echo 'set nocompatible' > ~/.vimrc
